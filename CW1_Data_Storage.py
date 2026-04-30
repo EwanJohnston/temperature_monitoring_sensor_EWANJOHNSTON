c468574906086e14cb76a556aa76417a66e6c800
EwanJohnston <ewanjohnston2006@gmail.com>
2026-04-30 02:59:22 +0100

    Delete CW1_Data_Storage.py

--- CW1_Data_Storage.py
+++
@@ -1,6 +0,0 @@
-def DataStorage(temp_c,current_datetime):
-    temp_c=str(temp_c)
-    str_current_datetime =str(current_datetime)
-    with open("temp" + str_current_datetime+".txt", "a") as f:
-        f.write(temp_c +"\n")
-        
